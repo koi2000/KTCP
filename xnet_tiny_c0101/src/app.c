101566feabf1bd31bcef2962631aa063823aac20
koi2000 <koi20000@163.com>
2023-08-12 09:44:26 +0800

    完成c0101

---
+++ xnet_tiny_c0101/src/app.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include"xnet_tiny.h"
+
+int main() {
+	xnet_init();
+	printf("xnet running\n");
+	while (1) {
+		xnet_poll();
+	}
+	return 0;
+}
---
+++ xnet_tiny_c0101/src/xnet_tiny/xnet_tiny.h
@@ -0,0 +1,29 @@
+#ifndef XNET_TINT_H
+#define XNET_TINY_H
+
+#include <stdint.h>
+// 收发数据包的最大大小
+#define XNET_CFG_PACKGE_MAX_SIZE 1516
+
+typedef enum _xnet_err_t {
+	XNET_ERR_OK = 0,
+	XNET_ERR_IO = -1,
+}xnet_err_t;
+
+// 网络数据结构
+typedef struct _xnet_packet_t {
+	// 包中有效数据大小
+	uint16_t size;
+	// 包中数据起始地址
+	uint8_t data;
+	// 最大负载数据量
+	uint8_t payload[XNET_CFG_PACKGE_MAX_SIZE];
+}xnet_packet_t;
+
+xnet_packet_t* xnet_alloc_for_send(uint16_t data_size);
+xnet_packet_t* xnet_alloc_for_read(uint16_t data_size);
+
+void xnet_init(void);
+void xnet_poll(void);
+
+#endif 
