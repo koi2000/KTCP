662138a314df383ecbbf4b3e17dc547e4b7b39e2
koi2000 <koi20000@163.com>
2023-08-16 16:51:49 +0800

    简易版http

--- xnet_tiny/src/xnet_app/xserver_http.c
+++ xnet_tiny/src/xnet_app/xserver_http.c
@@ -1,1 +1,24 @@
-﻿
+﻿#include "xserver_http.h"
+#include <string.h>
+#include <stdio.h>
+
+static uint8_t tx_buffer[1024];
+
+static xnet_err_t http_handler(xtcp_t* tcp, xtcp_conn_state_t state) {
+	if (state == XTCP_CONN_CONNECTED) {
+		printf("http connected.\n");
+	}
+	else if (state == XTCP_CONN_CLOSED) {
+		printf("http closed.\n");
+	}
+	else if (state == XTCP_CONN_DATA_RECV) {
+		uint8_t* data = tx_buffer;
+		uint16_t read_size = xtcp_read(tcp, tx_buffer, sizeof(tx_buffer));
+		while (read_size) {
+			uint16_t curr_size = xtcp_write(tcp, data, read_size);
+			data += curr_size;
+			read_size -= curr_size;
+		}
+	}
+	return XNET_ERR_OK;
+}
--- xnet_tiny/src/xnet_app/xserver_http.h
+++ xnet_tiny/src/xnet_app/xserver_http.h
@@ -1,1 +1,6 @@
-﻿
+﻿#ifndef XSERVER_HTTP_H
+#define XSERVER_HTTP_H
+#include "xnet_tiny.h"
+xnet_err_t xerver_http_create(uint16_t port);
+
+#endif // XSERVER_HTTP_H
