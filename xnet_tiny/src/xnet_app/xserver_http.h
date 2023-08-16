364b836de6d24ef0c66846a0c4183ff4372be8d0
koi2000 <koi20000@163.com>
2023-08-16 18:28:13 +0800

    新增简易版http server

--- xnet_tiny/src/xnet_app/xserver_http.h
+++ xnet_tiny/src/xnet_app/xserver_http.h
@@ -1,6 +1,8 @@
 ﻿#ifndef XSERVER_HTTP_H
 #define XSERVER_HTTP_H
 #include "xnet_tiny.h"
-xnet_err_t xerver_http_create(uint16_t port);
 
+#define XHTTP_DOC_DIR "htdocs"
+xnet_err_t xserver_http_create(uint16_t port);
+void xserver_http_run(void);
 #endif // XSERVER_HTTP_H
